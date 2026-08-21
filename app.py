198547b03db59a748a8d0fc3a9af273a7bd74e33
jaronanders <jyb6k2wtrw@privaterelay.appleid.com>
2026-08-21 16:02:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -173,7 +173,7 @@ def free_rooms():
         flash(f"Fehler beim Abrufen der Daten: {e}")
         return redirect(url_for("login"))
 
-    current_time = get_current_stunde_zeit()
+    current_time = 900
 
     # Räume herausfinden, die JETZT laut Stundenplan belegt sind
     occupied_room_names = set()
